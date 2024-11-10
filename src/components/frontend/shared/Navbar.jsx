c6f9c08fe728cc53913ebef02db2cc24612088ba
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-11-10 12:47:17 +0530

    Alter some of the links in the navbar frontend

--- src/components/frontend/shared/Navbar.jsx
+++ src/components/frontend/shared/Navbar.jsx
@@ -7,11 +7,6 @@ import Image from 'next/image';
 import waveNav from '../../../../public/frontend/SvgAssets/wave-nav.svg'; 
 import Link from "next/link";
 
-
-
-
-
-
 const Navbar = () => {
   const [isShopOpen, setIsShopOpen] = useState(false);
   const [isLearnOpen, setIsLearnOpen] = useState(false);
@@ -52,10 +47,15 @@ const Navbar = () => {
   {/* Right Side - Search, User, Cart Icons */}
   <div className="flex items-center space-x-4 md:hidden">
     <FiSearch className="w-6 h-6 cursor-pointer" />
+    <Link href={"/auth/signIn"}>
+    
     <FiUser className="w-6 h-6 cursor-pointer" />
+    </Link>
     <div className="relative">
+      <Link  href={"/product/cart"}>
+      
             <FiShoppingBag className="w-6 h-6 cursor-pointer" />
-        
+      </Link>
           </div>
   </div>
 </div>

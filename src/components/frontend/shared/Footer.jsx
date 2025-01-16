1cd8aa28254c891a61759a7d00a0fa731ef0bc2e
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-01-16 22:39:32 +0530

    Update the footer again

--- src/components/frontend/shared/Footer.jsx
+++ src/components/frontend/shared/Footer.jsx
@@ -1,83 +1,117 @@
 import React from "react";
-import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
-import { SiMastercard, SiVisa, SiApplepay } from "react-icons/si";
-import ApplePay from '../../../../public/icons/Method=ApplePay.png'
-import Discover from '../../../../public/icons/Discover.png'
-import Mastercard from '../../../../public/icons/Mastercard.png'
-import Visa from '../../../../public/icons/visa-logo.png'
+import Image from "next/image"; // Use for Next.js
+import ApplePay from '../../../../public/icons/Method=ApplePay.png';
+import Discover from '../../../../public/icons/Discover.png';
+import Mastercard from '../../../../public/icons/Mastercard.png';
+import Visa from '../../../../public/icons/visa-logo.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-10">
+    <footer className="bg-[#1A1A1A] text-white py-10 border-t border-gray-700">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
           {/* About Us */}
-          <div>
-            <h3 className="text-lg font-bold mb-4">About US</h3>
-            <p className="text-sm text-gray-400 mb-4">
-              Morbi cursus porttitor enim lobortis molestie. Duis gravida turpis
-              dui, eget bibendum magna congue nec.
+          <div className="space-y-4">
+            <h3 className="text-lg font-bold mb-2 border-b border-gray-600 pb-2">About Us</h3>
+            <p className="text-sm text-gray-400">
+              Morbi cursus porttitor enim lobortis molestie. Duis gravida turpis dui, eget bibendum magna congue nec.
             </p>
-            <p className="text-sm">
-              <span className="text-green-500">(219) 555–0114</span> or{" "}
+            <p className="text-sm text-gray-300">
+              <span className="text-green-500">(219) 555–0114</span> <br />
               <span className="text-green-500">Cleanveda@gmail.com</span>
             </p>
           </div>
 
           {/* My Account */}
-          <div>
-            <h3 className="text-lg font-bold mb-4">My Account</h3>
+          <div className="space-y-4">
+            <h3 className="text-lg font-bold mb-2  pb-2">My Account</h3>
             <ul className="text-sm space-y-2 text-gray-400">
-              <li>My Account</li>
-              <li>Order History</li>
-              <li>Shopping Cart</li>
-              <li>Wishlist</li>
+              <li className="hover:text-gray-200 cursor-pointer">My Account</li>
+              <li className="hover:text-gray-200 cursor-pointer">Order History</li>
+              <li className="hover:text-gray-200 cursor-pointer">Shopping Cart</li>
+              <li className="hover:text-gray-200 cursor-pointer">Wishlist</li>
             </ul>
           </div>
 
           {/* Helps */}
-          <div>
-            <h3 className="text-lg font-bold mb-4">Helps</h3>
+          <div className="space-y-4">
+            <h3 className="text-lg font-bold mb-2  pb-2">Helps</h3>
             <ul className="text-sm space-y-2 text-gray-400">
-              <li>Contact</li>
-              <li>FAQs</li>
-              <li>Terms & Condition</li>
-              <li>Privacy Policy</li>
+              <li className="hover:text-gray-200 cursor-pointer">Contact</li>
+              <li className="hover:text-gray-200 cursor-pointer">FAQs</li>
+              <li className="hover:text-gray-200 cursor-pointer">Terms & Conditions</li>
+              <li className="hover:text-gray-200 cursor-pointer">Privacy Policy</li>
             </ul>
           </div>
 
           {/* Proxy */}
-          <div>
-            <h3 className="text-lg font-bold mb-4">Proxy</h3>
+          <div className="space-y-4">
+            <h3 className="text-lg font-bold mb-2  pb-2">Proxy</h3>
             <ul className="text-sm space-y-2 text-gray-400">
-              <li>About</li>
-              <li>Shop</li>
-              <li>Product</li>
-              <li>Track Order</li>
+              <li className="hover:text-gray-200 cursor-pointer">About</li>
+              <li className="hover:text-gray-200 cursor-pointer">Shop</li>
+              <li className="hover:text-gray-200 cursor-pointer">Product</li>
+              <li className="hover:text-gray-200 cursor-pointer">Track Order</li>
             </ul>
           </div>
 
           {/* Categories */}
-          <div>
-            <h3 className="text-lg font-bold mb-4">Categories</h3>
+          <div className="space-y-4">
+            <h3 className="text-lg font-bold mb-2 pb-2">Categories</h3>
             <ul className="text-sm space-y-2 text-gray-400">
-              <li>Fruit & Vegetables</li>
-              <li>Meat & Fish</li>
-              <li>Bread & Bakery</li>
-              <li>Beauty & Health</li>
+              <li className="hover:text-gray-200 cursor-pointer">Fruit & Vegetables</li>
+              <li className="hover:text-gray-200 cursor-pointer">Meat & Fish</li>
+              <li className="hover:text-gray-200 cursor-pointer">Bread & Bakery</li>
+              <li className="hover:text-gray-200 cursor-pointer">Beauty & Health</li>
             </ul>
           </div>
         </div>
 
-        <div className="border-t border-gray-700 mt-8 pt-6 flex flex-col md:flex-row justify-between items-center">
-          {/* Footer Bottom */}
-          <p className="text-sm text-gray-400 mb-4 md:mb-0">
-            Clean Veda © 2025. All Rights Reserved
-          </p>
-          <div className="flex space-x-4">
-            <SiApplepay className="text-gray-400 text-xl" />
-            <SiVisa className="text-gray-400 text-xl" />
-            <SiMastercard className="text-gray-400 text-xl" />
+        <div className="border-t border-gray-700 mt-8 pt-6">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            {/* Footer Bottom */}
+            <p className="text-sm text-gray-400 mb-4 md:mb-0">
+              Clean Veda © 2025. All Rights Reserved
+            </p>
+            <div className="flex items-center space-x-4">
+              {/* Payment Icons */}
+              <div className="p-2 border border-gray-500 rounded-md">
+                <Image
+                  src={ApplePay}
+                  alt="Apple Pay"
+                  className="w-8 h-8 object-contain"
+                  width={32}
+                  height={32}
+                />
+              </div>
+              <div className="p-2 border border-gray-500 rounded-md">
+                <Image
+                  src={Visa}
+                  alt="Visa"
+                  className="w-8 h-8 object-contain"
+                  width={32}
+                  height={32}
+                />
+              </div>
+              <div className="px-3 py-2 border border-gray-500 rounded-md">
+                <Image
+                  src={Mastercard}
+                  alt="Mastercard"
+                  className="w-4 h-8 object-contain"
+                  width={32}
+                  height={32}
+                />
+              </div>
+              <div className="p-2 border border-gray-500 rounded-md">
+                <Image
+                  src={Discover}
+                  alt="Discover"
+                  className="w-8 h-8 object-contain"
+                  width={32}
+                  height={32}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>

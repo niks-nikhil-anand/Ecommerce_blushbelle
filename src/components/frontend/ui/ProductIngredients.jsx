f2f82a792942d1c62798dd1a0368560156994f64
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-01-17 10:45:54 +0530

    Added a Product ingredients section in the homepage

---
+++ src/components/frontend/ui/ProductIngredients.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainBanner from '../../../../public/frontend/ProductIngredients/Group_7.png'
+import icon1 from '../../../../public/frontend/ProductIngredients/icon1.png'
+import icon2 from '../../../../public/frontend/ProductIngredients/icon2.png'
+import icon3 from '../../../../public/frontend/ProductIngredients/icon3.png'
+import icon4 from '../../../../public/frontend/ProductIngredients/icon4.png'
+
+const ProductIngredients = () => {
+  return (
+    <div>ProductIngredients</div>
+  )
+}
+
+export default ProductIngredients

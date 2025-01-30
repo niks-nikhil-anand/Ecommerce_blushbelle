29f6453716cf13ac579a4c514ea3be901e5d865f
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-01-30 15:18:38 +0530

    Modify the catgeory section ui

--- src/components/frontend/ui/CategorySection.jsx
+++ src/components/frontend/ui/CategorySection.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { motion } from 'framer-motion';
 import Loader from '@/components/loader/loader';
-import Container from '@/components/utils/Container';
+import { FiArrowRight } from 'react-icons/fi'; // Importing the arrow icon from react-icons
 import Image from 'next/image';
 
 const CategoriesSection = () => {
@@ -40,15 +40,25 @@ const CategoriesSection = () => {
 
 
   return (
-    <div className="my-20 px-10">
-    <h2 className="text-sm sm:text-xl md:text-2xl mb-4 font-bold text-gray-900">
+    <div className="my-20 flex flex-col ">
+
+
+
+      <div className='flex gap-4 hover:cursor-pointer px-22 py-3 w-full justify-between'>
+      <h2 className="text-sm sm:text-xl md:text-2xl mb-4 font-bold text-gray-900">
       Popular Categories
     </h2>
+    <div className="flex items-center justify-center text-blue-500 hover:text-blue-700">
+      <p className="mr-2 text-sm font-medium">View All</p>
+      <FiArrowRight className="text-lg" /> {/* Add the arrow icon */}
+    </div>
+      </div>
+   
     <div className="flex gap-4 hover:cursor-pointer justify-center px-2 py-3 overflow-x-auto snap-x snap-mandatory sm:flex-wrap">
       {displayedCategories.map((category) => (
         <motion.div
           key={category.id}
-          className="relative flex-shrink-0 snap-center flex flex-col items-center bg-white shadow-md rounded-xl p-4 border hover:shadow-lg transition-all duration-300"
+          className="relative flex-shrink-0 snap-center flex flex-col items-center bg-white  rounded-xl p-4 border hover:shadow-lg transition-all duration-300"
         >
           <div className="flex flex-col items-center">
           <Image

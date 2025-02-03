cc33b56b06aa46e46f9837d11440f923f1767d93
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-02-03 15:13:51 +0530

    Testing

--- src/app/(Frontend)/aboutUs/page.jsx
+++ src/app/(Frontend)/aboutUs/page.jsx
@@ -31,7 +31,7 @@ export default function AboutPageOne() {
           className="flex flex-col space-y-8 pb-10 pt-12 md:pt-24"
         >
           <div className="max-w-max rounded-full border bg-gray-50 p-1 px-3">
-            <p className="text-xs font-semibold leading-normal md:text-sm">About the company</p>
+            <p className="text-xs font-semibold leading-normal md:text-sm">About the Cleanveda</p>
           </div>
           <p className="text-3xl font-bold text-gray-900 md:text-5xl md:leading-10">
             Made with love, right here in India

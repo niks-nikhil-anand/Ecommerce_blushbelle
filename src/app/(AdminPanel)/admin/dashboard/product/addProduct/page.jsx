cb0569f5c51aa5384ae60d5b3ccb6a8943cba661
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-02-09 19:37:04 +0530

    FIx bugs

--- src/app/(AdminPanel)/admin/dashboard/product/addProduct/page.jsx
+++ src/app/(AdminPanel)/admin/dashboard/product/addProduct/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import { motion } from 'framer-motion';
 import dynamic from 'next/dynamic';
